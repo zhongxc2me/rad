fcf94b9e9662a33735006a875f2f7c5c15986fea
zhongxc <cqzhongxc@hotmail.com>
2024-04-11 15:53:15 +0800

    安装MySQL脚本

---
+++ mysql/README.md
@@ -0,0 +1,37 @@
+### 配置自启动
+vim /etc/systemd/system/mysqld.service
+
+```
+[Unit]
+Description=MySQL Server
+After=network.target
+
+[Service]
+ExecStart=/usr/local/mysql/bin/mysqld_safe --user=mysql
+ExecStop=/usr/local/mysql/bin/mysqladmin shutdown
+ExecReload=/usr/local/mysql/bin/mysqladmin reload
+User=mysql
+Group=mysql
+
+[Install]
+WantedBy=multi-user.target
+```
+#### 重新加载 systemd
+sudo systemctl daemon-reload
+
+#### 设置开机自启
+sudo systemctl enable mysqld
+
+### 修改root远程访问权限
+```
+mysql -u root -p
+
+use mysql;
+
+select host, user from user;
+
+update user set host='%' where user='root';
+
+flush privileges;
+
+```
--- mysql/mysql_install.sh
+++ mysql/mysql_install.sh
@@ -5,16 +5,71 @@
 
 sudo yum update -y
 sudo yum groupinstall "Development Tools" -y
-sudo yum install -y perl perl-ExtUtils-MakeMaker
-
-yum install -y gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ \ 
-gcc-toolset-12-binutils gcc-toolset-12-annobin-annocheck \ 
+sudo yum install -y perl perl-ExtUtils-MakeMaker \
+gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ \
+gcc-toolset-12-binutils gcc-toolset-12-annobin-annocheck \
 gcc-toolset-12-annobin-plugin-gcc rpcgen 
 
-## https://mirrors.tuna.tsinghua.edu.cn/centos-stream/9-stream/CRB/x86_64/os/Packages/
-rpm -ivh https://mirrors.tuna.tsinghua.edu.cn/centos-stream/9-stream/CRB/x86_64/os/Packages/libtirpc-devel-1.3.3-2.el9.x86_64.rpm
+sudo dnf install -y make cmake bison ncurses ncurses-devel openssl-devel
 
-cd /usr/local/src/mysql-8.0.36 && mkdir build && cd build
+if ! rpm -qa | grep -q libtirpc-devel; then 
+    ## https://mirrors.tuna.tsinghua.edu.cn/centos-stream/9-stream/CRB/x86_64/os/Packages/
+    rpm -ivh https://mirrors.tuna.tsinghua.edu.cn/centos-stream/9-stream/CRB/x86_64/os/Packages/libtirpc-devel-1.3.3-2.el9.x86_64.rpm
+fi
+
+# 检查mysql用户组是否存在
+if ! getent group mysql > /dev/null; then
+    # 创建mysql用户组
+    sudo groupadd mysql
+    echo "用户组mysql已创建"
+else 
+    echo "用户组mysql已存在"
+fi
+
+if ! id -u mysql > /dev/null 2>&1; then 
+    # 创建mysql用户
+    sudo useradd -r -g mysql -s /sbin/nologin mysql
+    echo "用户mysql已创建"
+else 
+    echo "用户mysql已存在"
+fi
+
+cd /usr/local/src/
+
+# 判断MySQL源码文件是否存在
+if [ ! -f "mysql-boost-8.0.36.tar.gz" ]; then 
+    # MySQL源码文件不存在，则在线下载
+    echo ">"
+    echo "==============mysql-boost-8.0.36.tar.gz start download...================"
+    wget https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-boost-8.0.36.tar.gz
+    echo "download completed"
+fi
+
+# 检查目录是否已存在，存在则删除
+if [ -d "mysql-8.0.36" ]; then 
+    echo "mysql-8.0.36, 目录已存在，正在删除"
+    rm -rf mysql-8.0.36
+fi
+
+# 解压
+tar zxvf mysql-boost-8.0.36.tar.gz
+
+# 判断解压后的文件夹是否存在
+if [ ! -d "mysql-8.0.36" ]; then
+    echo 'mysql-8.0.36 folder does not exist'
+    exit
+fi
+
+cd mysql-8.0.36 && mkdir build && cd build
+
+# 检查cmake命令是否存在
+if command -v cmake &>/dev/null; then 
+    # cmake命令存在，继续执行后续操作
+    cmake --version
+else 
+    echo "cmake not install"
+    exit 1
+fi
 
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
          -DMYSQL_DATADIR=/usr/local/mysql/data \
@@ -34,6 +89,34 @@ sudo chown -R mysql:mysql /usr/local/mysql
 
 # 初始化数据库
 sudo /usr/local/mysql/bin/mysqld --initialize --user=mysql
+# temporary password is generated for root@localhost: Ij9JpyTheV.v
+
+# 启动MySQL服务
+/usr/local/mysql/support-files/mysql.server start
+
+#  配置环境变量
+echo 'export PATH=$PATH:/usr/local/mysql/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# 运行安全设置脚本
+echo "run the security setup script"
+echo "/usr/local/mysql/bin/mysql_secure_installation \n\n"
+
+
+echo "use mysql -uroot -p test login."
+echo "done."
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
